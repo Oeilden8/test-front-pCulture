93ebe99633605821d3ed86983255d76dc97ff852
Fanny <oeilden8@gmail.com>
2022-09-29 12:21:26 +0200

    installed the project and created my components with router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import "./App.css";
-
+import { Routes, Route } from "react-router-dom";
 import React from "react";
 import logo from "./logo-pass-culture.svg";
+import News from "./News/News";
+import AddNews from "./AddNews/AddNews";
 
 function App() {
   return (
@@ -9,6 +11,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
       </header>
+      <Routes>
+        <Route path="/" element={<News />} />
+        <Route path="/addNews" element={<AddNews />} />
+      </Routes>
     </div>
   );
 }
