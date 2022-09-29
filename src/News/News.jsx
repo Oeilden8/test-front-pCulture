dfdb102199bb25c2c8a621d0cb9a3a7767db56dc
Fanny <oeilden8@gmail.com>
2022-09-29 12:57:36 +0200

    page news done with basic css, but missing images

--- src/News/News.jsx
+++ src/News/News.jsx
@@ -1,7 +1,94 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "./News.css";
+import data from "../news.json";
 
 function News() {
-  return <div>News</div>;
+  const [category, setCategory] = useState("all");
+
+  //   fonctionne mais pas dans mon html, à voir si temps ?
+  //   console.log(data.news[0].images[0]);
+
+  const navigate = useNavigate();
+
+  // pour récupérer les catégories sans doublon
+  const newsType = data.news.map((type) => type.category);
+  const categoryName = [...new Set(newsType)];
+
+  const handleDate = (newsDate) => {
+    const options = {
+      weekday: "long",
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    };
+    const date = new Date(newsDate);
+    return date.toLocaleDateString("fr-FR", options);
+  };
+
+  return (
+    <div className="home-container">
+      <section className="select-container">
+        <h1>Nos dernières actualités</h1>
+        <label htmlFor="category-select">Trier par catégorie</label>
+        <select
+          name="catégorie"
+          className="select-category"
+          onChange={(e) => {
+            setCategory(e.target.value);
+          }}
+        >
+          <option value="all">Toutes les news</option>
+          {categoryName.map((name) => (
+            <option value={name} key={name}>
+              {name}
+            </option>
+          ))}
+        </select>
+      </section>
+      {category === "all" ? (
+        <section className="news-container">
+          {data.news.map((article) => (
+            <article className="news-card" key={article.title}>
+              <h2>{article.title.toUpperCase()}</h2>
+              <h3>{article.category}</h3>
+              <time dateTime={article.date}>{handleDate(article.date)}</time>
+              <p>{article.text}</p>
+              <div className="img-container">
+                {/* <img
+                  src={`../${article.images[0]}`}
+                  alt={`news ${article.title} de type ${article.category}  pass culture le ${article.date}`}
+                /> */}
+              </div>
+            </article>
+          ))}
+        </section>
+      ) : (
+        <section className="news-container">
+          {data.news
+            .filter((type) => type.category === category)
+            .map((article) => (
+              <article className="news-card" key={article.title}>
+                <h2>{article.title.toUpperCase()}</h2>
+                <h3>{article.category}</h3>
+                <time dateTime={article.date}>{handleDate(article.date)}</time>
+                <p>{article.text}</p>
+                <div className="img-container">
+                  {/* <img
+                    src={`../${article.images[0]}`}
+                    alt={`news ${article.category} de type  ${article.title} pass culture le ${article.date}`}
+                  /> */}
+                </div>
+              </article>
+            ))}
+        </section>
+      )}
+
+      <button className="button-nav" onClick={() => navigate("/addNews")}>
+        Créer une news
+      </button>
+    </div>
+  );
 }
 
 export default News;
