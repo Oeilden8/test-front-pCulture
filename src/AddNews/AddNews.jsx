a1a7b8fd514d4dabe5aaf8efe90d67e8128e6011
Fanny <oeilden8@gmail.com>
2022-09-29 13:22:28 +0200

    added form and some css

--- src/AddNews/AddNews.jsx
+++ src/AddNews/AddNews.jsx
@@ -1,7 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
+import "./AddNews.css";
 
 function AddNews() {
-  return <div>AddNews</div>;
+  const [actu, setActu] = useState({
+    title: "",
+    category: "",
+    date: "",
+    text: "",
+  });
+
+  const handleSubmitNews = (e) => {
+    e.preventDefault();
+    if (actu.title && actu.category && actu.date && actu.text) {
+      console.log("news", actu);
+    } else {
+      // si assez temps faire modal
+      alert("Merci de compléter tous les champs du formulaire");
+    }
+  };
+
+  return (
+    <div>
+      <h1>Créer une nouvelle actualité</h1>
+      <form className="form-news" onSubmit={handleSubmitNews}>
+        <label htmlFor="news title">
+          <input
+            type="text"
+            placeholder="TITRE"
+            value={actu.title}
+            onChange={(e) => setActu({ ...actu, title: e.target.value })}
+          />
+        </label>
+        <label htmlFor="news category">
+          {/* possibilité de faire un select option si catégories toujours les memes */}
+          <input
+            type="text"
+            placeholder="CATÉGORIE"
+            value={actu.category}
+            onChange={(e) => setActu({ ...actu, category: e.target.value })}
+          />
+        </label>
+        <label htmlFor="news date">
+          <input
+            type="date"
+            value={actu.date}
+            onChange={(e) => setActu({ ...actu, date: e.target.value })}
+          />
+        </label>
+        <label htmlFor="news description">
+          <textarea
+            name="description"
+            placeholder="DESCRIPTION"
+            value={actu.text}
+            onChange={(e) => setActu({ ...actu, text: e.target.value })}
+          />
+        </label>
+        <button type="submit" className="button-submit">
+          CRÉER UNE NEWS
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default AddNews;
